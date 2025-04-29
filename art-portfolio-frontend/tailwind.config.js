48d6bb631c0f1b779659b778097770e82c217e4a
Daria-Domnikova <dddomnikova@gmail.com>
2025-04-29 23:39:10 +0300

    [0.5] Reconfigured project  with adding Vite

--- art-portfolio-frontend/tailwind.config.js
+++ art-portfolio-frontend/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontFamily: {
-      'body' : ['"Inter"']
+    colors: {
+      'smoky' : '#0d0809',
+      'bone' : "#cacab9",
+      'cardinal': "#c4313b",
     },
-    extend: {},
+    fontFamily: {
+      'body' : ['"Noto Sans"']
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
